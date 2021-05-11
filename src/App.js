40fa9e1315af6ee22bd275b153c39f8465ade3a6
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-05-11 15:10:04 +0600

    basic setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  return <section className='section'>
+    <div className="title">
+      <h2><span>/</span>Review</h2>
+    </div>
+  </section>;
+}
+
+export default App;
